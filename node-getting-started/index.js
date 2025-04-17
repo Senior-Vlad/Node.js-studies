b55f0e420d5807bc46f39255440b350af26284eb
Senior-Vlad <133639757+Senior-Vlad@users.noreply.github.com>
2025-04-17 17:54:33 +0200

    Modules introduction
    myDateTime module created and used.

--- node-getting-started/index.js
+++ node-getting-started/index.js
@@ -1,8 +1,11 @@
 const http = require('http');
+var dt = require('./datemodule.js'); // Import the date module
 
 const server = http.createServer(/*function (then without => lambd*/(req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  //res.writeHead(200, { 'Content-Type':'text/plain; charset=utf-8'}),
+  res.write('Current time is: ' +dt.myDateTime()+'\n');
   res.end('Привіт, 132 Node.js!');
 });
 
