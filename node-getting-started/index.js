eb0dff4374afc03af4eac08e2f7cefa2addf2b14
Senior-Vlad <133639757+Senior-Vlad@users.noreply.github.com>
2025-04-17 17:18:55 +0200

    a little thing was clarified
    Done

--- node-getting-started/index.js
+++ node-getting-started/index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(/*function (then without => lambd*/(req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
   res.end('Привіт, 132 Node.js!');
