65ad03ecc2b2bb4cfd1b27e9c2a6a0cd4d63326e
Senior-Vlad <133639757+Senior-Vlad@users.noreply.github.com>
2025-04-17 17:00:18 +0200

    new file:   .vscode/settings.json 	new file:   node-getting-started/index.js 	new file:   node-getting-started/package.json

---
+++ node-getting-started/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain', 'charset=utf-8');
+  res.end('Привіт, Node.js!');
+});
+
+server.listen(3000, () => {
+  console.log('Сервер працює123 на http://localhost:3000');
+});
