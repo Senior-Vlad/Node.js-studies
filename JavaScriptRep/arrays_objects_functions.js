5d1234ab5da8bc89178c2cadae3ef1d30e859d33
Senior-Vlad <133639757+Senior-Vlad@users.noreply.github.com>
2025-04-19 17:09:57 +0200

    Training

---
+++ JavaScriptRep/arrays_objects_functions.js
@@ -0,0 +1,53 @@
+let order = {
+  customer: {
+    customerName: "John Doe",
+    customerProduct: "Smartphone",
+    customerEmail: "email@gmail.com",
+    customerID: 123456,
+    customerAddress: "123 Main St, Anytown, USA",
+  },
+  items: {
+    item1: {
+      productName: "Smartphone",
+      productPrice: 699.99,
+      productQuantity: 1,
+    },
+    item2: {
+      productName: "Laptop",
+      productPrice: 1299.99,
+      productQuantity: 1,
+    },
+    item3: {
+      productName: "Headphones",
+      productPrice: 199.99,
+      productQuantity: 2,
+    },
+  }
+};
+
+const users = [ // array of objects
+    {id: 0, name: "Jane Smith", active: true},
+    {id: 1, name: "Kohn Doe", active: true},
+    {id: 2, name: "Jane Smith", active: false},
+    {id: 3, name: "Sam Brown", active: true},
+]
+
+const activeUsers = users.filter(user => user.active);/*.map(user =>{
+    return {id: user.id, name: user.name, active: user.active}
+});*/
+console.log(activeUsers);
+console.log("\n\n");
+const findUser = users.find(user => user.name === "Jane Smith");
+//let logged = user.isLoggedIn ? "Welcome back, "+user.Uname+"!" : "Please log in.";
+console.log(findUser ? `User found - id: ${findUser.id} ${findUser.name}` : "User not found!");
+console.log("\n");
+const updateUser = users.map(user => {
+    return user.name === "Sam Brown" ?{...user, name:"123",active:false} :user // update user with name Sam Brown to inactive (active: false)
+});
+console.log(updateUser)
+
+const InActiveUsers = updateUser.filter(user => !user.active);
+console.log(InActiveUsers); // filter inactive users
+
+const SortedUsers = [...updateUser].sort((a,b) => a.name.localeCompare(b.name)); // sort users by name
+console.log(SortedUsers);
