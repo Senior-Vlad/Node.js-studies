3f3788293c8ce26f6687d6c9018a91214aa0f763
merzeef <133639757+Senior-Vlad@users.noreply.github.com>
2025-04-24 04:09:58 +0200

    Delete PROJECT directory
    Transfer to private

--- PROJECT/src/routes/subscriptionRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { createSubscription } = require("../controllers/subscriptionController");
-const { subscriptionValidation } = require("../utils/validators");
-
-router.post("/subscriptions", subscriptionValidation, createSubscription);
-
-module.exports = router;
